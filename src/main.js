1d745e8c137da3baa7a271f96a5686c0b0cf839b
YeokShi <148997928+YeokShi@users.noreply.github.com>
2023-11-10 22:14:35 +0800

    create starting point. from html, css to javascript

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import '../components/style.css'
+import '../components/header.css'
+import '../components/nav.css'
+import '../components/hero.css'
+import '../components/about.css'
+import '../components/skills.css'
+import '../components/training-work.css'
+import '../components/work.css'
+import '../components/footer.css'
+import '../components/util/util.css'
+
