56e3c7284209ddd2c41a7926b38f3bd845bff183
YeokShi <148997928+YeokShi@users.noreply.github.com>
2023-11-12 02:31:24 +0800

    make nav-link interactive. updated the javascript code.  Close the menu when a link is clicked

--- src/main.js
+++ src/main.js
@@ -10,15 +10,37 @@ import '../components/footer.css'
 import '../components/util/util.css'
 
 
-const menuBtn = document.querySelector('.nav-menu-bar');
-const menuLink = document.querySelector('.nav-menu-link');
-const closeBtn = document.querySelector('.closeBtn');
+// const menuBtn = document.querySelector('.nav-menu-bar');
+// const menuLink = document.querySelector('.nav-menu-link');
+// const closeBtn = document.querySelector('.closeBtn');
 
-menuBtn.addEventListener('click', ()=>{
-  menuLink.classList.add('nav-menu-content');
-  console.log(menuBtn);
-});
+// menuBtn.addEventListener('click', ()=>{
+//   menuLink.classList.add('nav-menu-content');
+//   console.log(menuBtn);
+// });
 
-closeBtn.addEventListener('click', ()=>{
-  menuLink.classList.remove('nav-menu-content');
+// closeBtn.addEventListener('click', ()=>{
+//   menuLink.classList.remove('nav-menu-content');
+// });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const menuBtn = document.querySelector('.nav-menu-bar');
+  const menuLink = document.querySelector('.nav-menu-link');
+  const closeBtn = document.querySelector('.closeBtn');
+  const navLinks = document.querySelectorAll('.nav-link a');
+
+  menuBtn.addEventListener('click', () => {
+    menuLink.classList.toggle('nav-menu-content');
+  });
+
+  closeBtn.addEventListener('click', () => {
+    menuLink.classList.remove('nav-menu-content');
+  });
+
+  
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      menuLink.classList.remove('nav-menu-content');
+    });
+  });
 });
