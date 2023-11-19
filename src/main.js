20e73e1e48f409df3afbf4f32cffbccaf483a6ec
YeokShi <148997928+YeokShi@users.noreply.github.com>
2023-11-20 02:01:11 +0800

    move the files to designated folder

--- src/main.js
+++ src/main.js
@@ -1,59 +1,54 @@
-import '../components/style.css'
-import '../components/header.css'
-import '../components/nav.css'
-import '../components/hero.css'
-import '../components/about.css'
-import '../components/skills.css'
-import '../components/training-cert.css'
-import '../components/work-project.css'
-import '../components/footer.css'
-import '../components/util/util.css'
+import "../components/style.css";
+import "../components/header.css";
+import "../components/nav.css";
+import "../components/hero.css";
+import "../components/about.css";
+import "../components/skills.css";
+import "../components/training-cert.css";
+import "../components/work-project.css";
+import "../components/footer.css";
+import "../components/util/util.css";
 
-import {skills, skills2 } from './data/skills/skills.js'
-import { skillsHtml } from './data/skills/skillsHtml.js'
+import { skills, skills2 } from "./skills/skills.js";
+import { skillsHtml } from "./skills/skillsHtml.js";
 
+import { toggleDescription } from "./work/work-proj.js";
 
- document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener("DOMContentLoaded", function () {
   // for skills section
   skillsHtml();
+  toggleDescription();
 
   // nav menu
-  const sections =  document.querySelectorAll('.main');
-  const menuBtn = document.querySelector('.nav-menu-bar');
-  const menuLink = document.querySelector('.nav-menu-link');
-  const closeBtn = document.querySelector('.closeBtn');
-  const navLinks = document.querySelectorAll('.nav-link a');
-  
-  menuBtn.addEventListener('click', () => {
-    menuLink.classList.add('nav-menu-content');
+  const sections = document.querySelectorAll(".main");
+  const menuBtn = document.querySelector(".nav-menu-bar");
+  const menuLink = document.querySelector(".nav-menu-link");
+  const closeBtn = document.querySelector(".closeBtn");
+  const navLinks = document.querySelectorAll(".nav-link a");
+
+  menuBtn.addEventListener("click", () => {
+    menuLink.classList.add("nav-menu-content");
     menuLink.style.width = "200px";
-    sections.forEach((section)=>{
-      section.style.opacity = '0.2';
-    })
-    
-    
+    sections.forEach((section) => {
+      section.style.opacity = "0.2";
+    });
   });
-  
-  closeBtn.addEventListener('click', () => {
-    menuLink.classList.remove('nav-menu-content');
-    sections.forEach((section)=>{
-      section.style.opacity = '1';
-    })
+
+  closeBtn.addEventListener("click", () => {
+    menuLink.classList.remove("nav-menu-content");
+    sections.forEach((section) => {
+      section.style.opacity = "1";
+    });
   });
-  
-  
-  navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-      menuLink.classList.remove('nav-menu-content');
-      sections.forEach((section)=>{
-        section.style.opacity = '1';
-      })
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      menuLink.classList.remove("nav-menu-content");
+      sections.forEach((section) => {
+        section.style.opacity = "1";
+      });
     });
   });
 
   // end of nav menu
- } )
-
-
-
-
+});
